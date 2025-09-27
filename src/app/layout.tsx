4bf225b902f37de9e402a4b1b0131d6f779b2e07
Wolf-Root <mobilemax363@gmail.com>
2025-09-27 16:12:14 +0100

    Change favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,24 +24,15 @@ export const metadata: Metadata = {
 
   icons: {
     icon: [
+      { url: "/favicon/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon/android-chrome-192x192.png", sizes: "192x192", type: "image/png" },
+      { url: "/favicon/android-chrome-512x512.png", sizes: "512x512", type: "image/png" },
       { url: "/favicon/favicon.ico" },
-      { url: "/favicon/favicon-96x96.png", type: "image/png" },
-      { url: "/favicon/favicon.svg", type: "image/svg+xml" },
     ],
     apple: [{ url: "/favicon/apple-touch-icon.png", sizes: "180x180", type: "image/png" }],
-    other: [
-      {
-        url: "/favicon/web-app-manifest-192x192.png",
-        sizes: "192x192",
-        type: "image/png",
-      },
-      {
-        url: "/favicon/web-app-manifest-512x512.png",
-        sizes: "512x512",
-        type: "image/png",
-      },
-    ],
   },
+
   manifest: "/favicon/site.webmanifest",
 
   // Open Graph (Facebook, LinkedIn)
